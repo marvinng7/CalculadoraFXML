3f84a1888994fda8cee604f2a4a02ad2b3a49b09
marvinng7 <marvinngabu77@gmail.com>
2022-01-16 22:21:47 +0000

    css
    Uso de css

--- src/main/java/dad/calculadorafxml/App.java
+++ src/main/java/dad/calculadorafxml/App.java
@@ -4,27 +4,23 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-
 public class App extends Application {
 
-	
 	private Controller controlador;
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-		
+
 		controlador = new Controller();
-		Scene scene = new Scene(controlador.getView(),400,300);
+		controlador.getView().getStylesheets().add("/css/clasica.css");
+		//controlador.getView().getStylesheets().add("/css/moderna.css");
+		Scene scene = new Scene(controlador.getView());
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("CalculadoraFXML");
 		primaryStage.show();
-		
-		
+
 	}
-	
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/main/java/dad/calculadorafxml/Controller.java
+++ src/main/java/dad/calculadorafxml/Controller.java
@@ -10,76 +10,78 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class Controller implements Initializable {
-	
+
 	private Calculadora calculadora = new Calculadora();
-	
+
 	@FXML
-    private Button borrarAll;
+	private Button borrarAll;
 
-    @FXML
-    private Button buttonBorrar;
+	@FXML
+	private Button buttonBorrar;
 
-    @FXML
-    private Button buttonCero;
+	@FXML
+	private Button buttonCero;
 
-    @FXML
-    private Button buttonCinco;
+	@FXML
+	private Button buttonCinco;
 
-    @FXML
-    private Button buttonComa;
+	@FXML
+	private Button buttonComa;
 
-    @FXML
-    private Button buttonCuatro;
+	@FXML
+	private Button buttonCuatro;
 
-    @FXML
-    private Button buttonDivision;
+	@FXML
+	private Button buttonDivision;
 
-    @FXML
-    private Button buttonDos;
+	@FXML
+	private Button buttonDos;
 
-    @FXML
-    private Button buttonIgual;
+	@FXML
+	private Button buttonIgual;
 
-    @FXML
-    private Button buttonNueve;
+	@FXML
+	private Button buttonNueve;
 
-    @FXML
-    private Button buttonOcho;
+	@FXML
+	private Button buttonOcho;
 
-    @FXML
-    private Button buttonProducto;
+	@FXML
+	private Button buttonProducto;
 
-    @FXML
-    private Button buttonResta;
+	@FXML
+	private Button buttonResta;
 
-    @FXML
-    private Button buttonSeis;
+	@FXML
+	private Button buttonSeis;
 
-    @FXML
-    private Button buttonSiete;
+	@FXML
+	private Button buttonSiete;
 
-    @FXML
-    private Button buttonSumar;
+	@FXML
+	private Button buttonSumar;
 
-    @FXML
-    private Button buttonTres;
+	@FXML
+	private Button buttonTres;
 
-    @FXML
-    private Button buttonUno;
+	@FXML
+	private Button buttonUno;
 
-    @FXML
-    private GridPane view;
+	@FXML
+	private GridPane view;
 
-    @FXML
-    private TextField viewText;
-    
-    private StringProperty viewProperty = new SimpleStringProperty();
-    
-    public Controller() throws IOException {
+	@FXML
+	private TextField viewText;
+
+	private StringProperty viewProperty = new SimpleStringProperty();
+
+	public Controller() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Cal.fxml"));
 		loader.setController(this);
 		loader.load();
@@ -87,32 +89,29 @@ public class Controller implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 		viewText.textProperty().bind(viewProperty);
-		
-		buttonCero.setOnAction(e->onActionNumber(buttonCero.getText()));
-		buttonUno.setOnAction(e->onActionNumber(buttonUno.getText()));
-		buttonDos.setOnAction(e->onActionNumber(buttonDos.getText()));
-		buttonTres.setOnAction(e->onActionNumber(buttonTres.getText()));
-		buttonCuatro.setOnAction(e->onActionNumber(buttonCuatro.getText()));
-		buttonCinco.setOnAction(e->onActionNumber(buttonCinco.getText()));
-		buttonSeis.setOnAction(e->onActionNumber(buttonSeis.getText()));
-		buttonSiete.setOnAction(e->onActionNumber(buttonSiete.getText()));
-		buttonOcho.setOnAction(e->onActionNumber(buttonOcho.getText()));
-		buttonNueve.setOnAction(e->onActionNumber(buttonNueve.getText()));
-		
-		buttonSumar.setOnAction(e->onOperacionesAction("suma",'+'));
-		buttonResta.setOnAction(e->onOperacionesAction("resta",'-'));
-		buttonProducto.setOnAction(e->onOperacionesAction("multiplicacion",'*'));
-		buttonDivision.setOnAction(e->onOperacionesAction("division",'/'));
-		buttonIgual.setOnAction(e->onOperacionesAction("igual",'='));
-		buttonComa.setOnAction(e->onOperacionesAction("coma",'.'));
-		borrarAll.setOnAction(e->onOperacionesAction("borrarAll",'0'));
-		buttonBorrar.setOnAction(e->onOperacionesAction("borrar",'0'));
-		
-		
-		
-		
+
+		buttonCero.setOnAction(e -> onActionNumber(buttonCero.getText()));
+		buttonUno.setOnAction(e -> onActionNumber(buttonUno.getText()));
+		buttonDos.setOnAction(e -> onActionNumber(buttonDos.getText()));
+		buttonTres.setOnAction(e -> onActionNumber(buttonTres.getText()));
+		buttonCuatro.setOnAction(e -> onActionNumber(buttonCuatro.getText()));
+		buttonCinco.setOnAction(e -> onActionNumber(buttonCinco.getText()));
+		buttonSeis.setOnAction(e -> onActionNumber(buttonSeis.getText()));
+		buttonSiete.setOnAction(e -> onActionNumber(buttonSiete.getText()));
+		buttonOcho.setOnAction(e -> onActionNumber(buttonOcho.getText()));
+		buttonNueve.setOnAction(e -> onActionNumber(buttonNueve.getText()));
+
+		buttonSumar.setOnAction(e -> onOperacionesAction("suma", '+'));
+		buttonResta.setOnAction(e -> onOperacionesAction("resta", '-'));
+		buttonProducto.setOnAction(e -> onOperacionesAction("multiplicacion", '*'));
+		buttonDivision.setOnAction(e -> onOperacionesAction("division", '/'));
+		buttonIgual.setOnAction(e -> onOperacionesAction("igual", '='));
+		buttonComa.setOnAction(e -> onOperacionesAction("coma", '.'));
+		borrarAll.setOnAction(e -> onOperacionesAction("borrarAll", '0'));
+		buttonBorrar.setOnAction(e -> onOperacionesAction("borrar", '0'));
+
 	}
 
 	private void onOperacionesAction(String string, char c) {
@@ -122,20 +121,20 @@ public class Controller implements Initializable {
 		case "multiplicacion":
 		case "igual":
 		case "division":
-			
+
 			calculadora.operar(c);
 			break;
-			
+
 		case "coma":
 			calculadora.insertarComa();
 			break;
-			
+
 		case "borrarAll":
 			calculadora.borrarTodo();
 
 		case "borrar":
 			calculadora.borrar();
-			
+
 		default:
 			break;
 		}
@@ -146,7 +145,7 @@ public class Controller implements Initializable {
 		calculadora.insertar(text.charAt(0));
 		viewProperty.set(calculadora.getPantalla());
 	}
-	
+
 	public GridPane getView() {
 		return view;
 	}
